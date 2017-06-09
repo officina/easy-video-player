972198b665f630af95e0c42434ce6b54771836a2
Aidan Follestad <Aidan.Follestad@target.com>
2017-06-09 15:29:14 -0500

    Tweaks

--- library/src/main/java/com/afollestad/easyvideoplayer/Util.java
+++ library/src/main/java/com/afollestad/easyvideoplayer/Util.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 /** @author Aidan Follestad (afollestad) */
 class Util {
 
-  public static String getDurationString(long durationMs, boolean negativePrefix) {
+  static String getDurationString(long durationMs, boolean negativePrefix) {
     return String.format(
         Locale.getDefault(),
         "%s%02d:%02d",
@@ -21,7 +21,7 @@ class Util {
             - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(durationMs)));
   }
 
-  public static boolean isColorDark(int color) {
+  static boolean isColorDark(int color) {
     double darkness =
         1
             - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color))
@@ -29,7 +29,7 @@ class Util {
     return darkness >= 0.5;
   }
 
-  public static int adjustAlpha(int color, @SuppressWarnings("SameParameterValue") float factor) {
+  static int adjustAlpha(int color, @SuppressWarnings("SameParameterValue") float factor) {
     int alpha = Math.round(Color.alpha(color) * factor);
     int red = Color.red(color);
     int green = Color.green(color);
@@ -37,11 +37,11 @@ class Util {
     return Color.argb(alpha, red, green, blue);
   }
 
-  public static int resolveColor(Context context, @AttrRes int attr) {
+  static int resolveColor(Context context, @AttrRes int attr) {
     return resolveColor(context, attr, 0);
   }
 
-  public static int resolveColor(Context context, @AttrRes int attr, int fallback) {
+  private static int resolveColor(Context context, @AttrRes int attr, int fallback) {
     TypedArray a = context.getTheme().obtainStyledAttributes(new int[] {attr});
     try {
       return a.getColor(0, fallback);
@@ -50,7 +50,7 @@ class Util {
     }
   }
 
-  public static Drawable resolveDrawable(Context context, @AttrRes int attr) {
+  static Drawable resolveDrawable(Context context, @AttrRes int attr) {
     return resolveDrawable(context, attr, null);
   }
 
